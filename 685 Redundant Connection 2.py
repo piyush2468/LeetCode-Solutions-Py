504ad43c9f6d3f657d4c02e9fa72e942c274d880
Piyush Raj Singh <piyushraj246800@gmail.com>
2020-03-31 22:11:52 +0530

    Update 685 Redundant Connection 2.py

--- 685 Redundant Connection 2.py
+++ 685 Redundant Connection 2.py
@@ -1,19 +1,39 @@
 class Solution:
-    def findRedundantDirectedConnection(self, edges: List[List[int]]) -> List[int]:
-        n = len(edges)
-        parent = dict(zip(range(1,n+1), range(1,n+1)))
-        last_cycle_edge = candidate_edge = candidate = None
-        def root(v):
-            while parent[v] != v:
-                v = parent[v]
-            return v
-        for e in edges:
-            v, w = e
-            if parent[w] != w: # case 1: w has 2 parents
-                candidate_edge, candidate = e, w
-                continue
-            if root(v) == w: # case 2: [v, w] makes a cycle
-                last_cycle_edge = e
-                continue
-            parent[w], n = v, n-1
-        return [parent[candidate], candidate] if n > 1 else (candidate_edge or last_cycle_edge)
+    def findRedundantDirectedConnection(self, edges):
+        n, p1, p2, c = len(edges), None, None, None
+        p = [0] * (n+1)
+        for i, (u,v) in enumerate(edges):
+            if p[v]: p1, p2, c, edges[i][0] = p[v], u, v, 0
+            else: p[v] = u
+        p = list(range(n+1))
+        
+        def find(x):
+            if p[x] != x: p[x] = find(p[x])
+            return p[x]
+        
+        for u, v in edges:
+            if u:
+                pu, pv = find(u), find(v)
+                if pu == pv: return p1 and [p1, c] or [u, v]
+                else: p[pv] = pu
+        return [p2, c]
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
